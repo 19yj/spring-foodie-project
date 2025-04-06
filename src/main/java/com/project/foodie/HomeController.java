cbf432a429e3df2b8047951caebef3fcebd588ba
joey <joey@bixtech.co>
2025-04-06 21:34:13 +0800

    Created HomeController to render index.html when running the application & Added dependencies and JPA properties

---
+++ src/main/java/com/project/foodie/HomeController.java
@@ -0,0 +1,13 @@
+package com.project.foodie;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index.html";
+    }
+}
